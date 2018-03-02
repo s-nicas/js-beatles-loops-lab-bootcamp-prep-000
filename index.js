765a7809577c67055af21e3ca0d988691f6dcafe
Sonia  D <sduarte22@gmail.com>
2018-03-02 20:52:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(arrayMusicians, arrayInstruments){
+ var results =[];
+ for(var i=0; i<arrayMusicians.length; i++){
+
+    results.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+}
+  
+ return results;
+}
+
+function johnLennonFacts(array){
+  while(array.length>0){
+    array.push("!!!");
+  }
+}
