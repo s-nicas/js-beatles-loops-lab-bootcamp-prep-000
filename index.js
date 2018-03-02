cc228f36dc87f7d70cd97c650ec3b7dce0f46f99
Sonia  D <sduarte22@gmail.com>
2018-03-02 21:21:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,3 +41,20 @@ function  iLoveTheBeatles (number){
 //   while (i>=15);
 //   return results;
 // }
+
+function  iLoveTheBeatles (number){
+
+  var results =[];
+  var i = 0;
+  
+  do{
+    i++;
+    results.push("I love the Beatles!");
+  }
+
+  while (i<number);
+  if (number<15){  
+  console.log(results);
+}}
+
+iLoveTheBeatles(14)
